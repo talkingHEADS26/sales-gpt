4bcfe4b4986b8fa962bcb09d2c68811a0bf73f77
talkingHEADS26 <digital-marketing@talkingheads.academy>
2026-06-02 10:50:57 +0200

    Make landing the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,2 @@
-import type { Metadata } from "next";
-
-import HomePageContent from "./homepage";
-
-export const metadata: Metadata = {
-  description:
-    "talkingHEADS Sales Trainer trainiert reale Verkaufsgespräche mit direktem Feedback, klarer Entwicklung und skalierbaren Lizenzen für Einzelpersonen und Teams.",
-};
-
-export default function HomePage() {
-  return <HomePageContent />;
-}
+export { metadata } from "./landing/page";
+export { default } from "./landing/page";
